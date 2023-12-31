02052499eb8c45a7ea4ba969109c8249bf935cb7
gyorutan <waou1998@gmail.com>
2024-01-01 03:41:50 +0900

    update

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -1,53 +1,55 @@
-import {NextResponse} from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 import prisma from "@/lib/prismadb";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
-export const POST = async (request: Request) => {
-    try {
-        const {studentId, password} = await request.json();
+export const POST = async (request: NextRequest) => {
+  try {
+    const { studentId, password } = await request.json();
 
-        const user = await prisma.user.findUnique({
-            where: {
-                studentId,
-            },
-        });
+    const user = await prisma.user.findUnique({
+      where: {
+        studentId,
+      },
+    });
 
-        if (!user) {
-            return {success: false, message: '등록되지 않은 학번입니다'};
-        }
-
-        const comparePassword = await bcrypt.compare(password, user.password!);
-
-        if (!comparePassword) {
-            return {
-                success: false,
-                message: '비밀번호가 일치하지 않습니다',
-            };
-        }
-
-        const payload = {
-            id: user.id,
-            fullname: user.fullname,
-            username: user.username,
-            studentId: user.studentId,
-            email: user.email,
-        }
-
-        const token = jwt.sign(payload, process.env.JWT_SECRET_KEY!, {expiresIn: "1h"});
-
-        const response = NextResponse.json({
-            success: true,
-            message: "로그인에 성공하였습니다"
-        })
-
-        response.cookies.set("user_token", token, {
-            httpOnly: true
-        });
-
-        return response;
-    } catch (error) {
-        console.log(error);
-        return NextResponse.json({success: false, message: "알 수 없는 오류"});
+    if (!user) {
+      return { success: false, message: "등록되지 않은 학번입니다" };
     }
-}
+
+    const comparePassword = await bcrypt.compare(password, user.password!);
+
+    if (!comparePassword) {
+      return {
+        success: false,
+        message: "비밀번호가 일치하지 않습니다",
+      };
+    }
+
+    const payload = {
+      id: user.id,
+      fullname: user.fullname,
+      username: user.username,
+      studentId: user.studentId,
+      email: user.email,
+    };
+
+    const token = jwt.sign(payload, process.env.JWT_SECRET_KEY!, {
+      expiresIn: "1h",
+    });
+
+    const response = NextResponse.json({
+      success: true,
+      message: "로그인에 성공하였습니다",
+    });
+
+    response.cookies.set("user_token", token, {
+      httpOnly: true,
+    });
+
+    return response;
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ success: false, message: "알 수 없는 오류" });
+  }
+};
--- app/api/auth/signup/route.ts
+++ app/api/auth/signup/route.ts
@@ -1,30 +1,34 @@
-import {NextResponse} from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 import prisma from "@/lib/prismadb";
 import bcrypt from "bcrypt";
 
-export const POST = async (request: Request) => {
-    try {
-        const {sei, mei, username, studentId, password} = await request.json();
+export const POST = async (request: NextRequest) => {
+  try {
+    const { sei, mei, username, studentId, password } = await request.json();
 
-        const hashedPassword = await bcrypt.hash(password, 10);
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-        const createdUser = await prisma.user.create({
-            data: {
-                sei,
-                mei,
-                fullname: `${sei}` + ' ' + `${mei}`,
-                username,
-                studentId,
-                email: `${studentId}` + '@shinshu-u.ac.jp',
-                password: hashedPassword
-            }
-        })
+    const createdUser = await prisma.user.create({
+      data: {
+        sei,
+        mei,
+        fullname: `${sei}` + " " + `${mei}`,
+        username,
+        studentId,
+        email: `${studentId}` + "@shinshu-u.ac.jp",
+        password: hashedPassword,
+      },
+    });
 
-        console.log(createdUser);
+    console.log(createdUser);
 
-        return NextResponse.json({success: true, message: '회원가입에 성공하였습니다', createdUser});
-    } catch (error) {
-        console.log(error);
-        return NextResponse.json({success: false, message: "알 수 없는 오류"});
-    }
-}
+    return NextResponse.json({
+      success: true,
+      message: "회원가입에 성공하였습니다",
+      createdUser,
+    });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ success: false, message: "알 수 없는 오류" });
+  }
+};
